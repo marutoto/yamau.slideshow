1545803ebb4e97501e59030cf8cd795af9a58ca7
yamauchi <k.yamauchi@core-tech.jp>
2013-03-05 18:45:30 +0900

    大幅に修正

--- jquery.mySlideshow.js
+++ jquery.mySlideshow.js
@@ -7,11 +7,130 @@
 
 		var opts = $.extend({}, $.fn.mySlideshow.defaults, options);
 
+		// スライドショー用のブロック要素を1つずつ回す
 		elements.each(function() {
 
-			var $img    = $(this);
-			var current = 0;
-
+			// オプション予定
+			var opt_li_width = 200;
+			var opt_li_x_margin = 30;
+			var opt_r_l_disp = 0;
+			
+			var opt_li_height = 500;
+			var opt_li_y_margin = 50;
+			
+			var opt_silde_time = 700;
+			
+			var opt_auto_slide_interval = 0;
+			
+			var opt_slide_guide = true;
+			
+			
+			// 初期化
+			var $slideshow  = $(this);
+			var disp_number = 0;
+			var image_cnt   = 0;
+			
+			// セレクタの要素内へスライドショーを描写する
+			var html;
+			html  = '<div class="prev-button"></div>';
+			html += '<ul class="image-area">';
+			for(var i in opts.images) {
+				html += '<li><p><img src="'+opts.images[i]+'"></p></li>';
+				image_cnt++;
+			}
+			html += '</ul>';
+			html += '<div class="next-button"></div>';
+			html += '<div class="clear"></div>';
+			
+			if(opt_slide_guide) {
+				
+			}
+			
+			$slideshow.append(html);
+			
+			// positionのleft位置を設定する
+			var ul_width = opt_li_width + opt_li_x_margin * 2;
+			
+			$slideshow.children('ul').css('width', ul_width+'px');
+			$slideshow.children('ul').children('li').css('width', opt_li_width+'px');
+			
+			var ul_height = opt_li_height + opt_li_y_margin * 2;
+			
+			$slideshow.children('ul').css('height', ul_height+'px');
+			$slideshow.children('ul').children('li').css('height', opt_li_height+'px');
+			$slideshow.children('ul').children('li').css('top', opt_li_y_margin+'px');
+			
+			var left = opt_li_x_margin;
+			
+			$slideshow.children('ul').children('li').each(function() {
+				
+				$(this).css('left', left+'px');
+				left += opt_li_width + opt_li_x_margin - opt_r_l_disp;
+				
+			});
+			
+			
+			// 画面をロード後、画像をリサイズする
+			$(window).bind('load', function() {
+			
+				$slideshow.children('ul').children('li').children('p').children('img').each(function() {
+					
+					var img_w = $(this).width();
+					var img_h = $(this).height();
+					
+					// liの縦幅・横幅指定のうち、短いほうに合わせて画像をリサイズする
+					if(opt_li_width >= opt_li_height) {
+						if(opt_li_height < img_h) {
+							$(this).height(opt_li_height); // 縦幅指定が短いので、heightを基準に比率を保持してリサイズ
+						}
+					} else {
+						if(opt_li_width < img_w) {
+							$(this).width(opt_li_width);   // 横幅指定が短いので、widthを基準に比率を保持してリサイズ
+						}
+					}
+					
+				});
+			
+			});
+			
+			
+			// ひとつ前の画像へ戻るイベントを監視
+			$slideshow.children('.prev-button').click(function() {
+				
+				if(disp_number > 0) {
+					disp_number--;
+					
+					var movement = $slideshow.children('ul').children('li:eq('+disp_number+')').position().left - opt_li_x_margin;
+					
+					$slideshow.children('ul').children('li').stop().animate({
+						"left": '+=' + Math.abs(movement) + 'px'
+					}, opt_silde_time, function() {
+						
+					});
+				}
+			});
+			
+			// ひとつ後の画像へ進むイベントを監視
+			$slideshow.children('.next-button').click(function() {
+				
+				if(disp_number < image_cnt-1) {
+					disp_number++;
+					
+					var movement = $slideshow.children('ul').children('li:eq('+disp_number+')').position().left - opt_li_x_margin;
+					
+					$slideshow.children('ul').children('li').stop().animate({
+						"left": '-=' + Math.abs(movement) + 'px'
+					}, opt_silde_time, function() {
+						
+					});
+				}
+				
+			});
+			
+			//TODO:autoイベントをつくる
+			
+			
+/*
 			// 「prev_event」「next_event」「goto_event」を定義・監視
 			// 監視しているイベントが起きたら、コールバック関数が起動
 			$img.bind('prev_event', function() {
@@ -74,7 +193,20 @@
 			$img.bind('start_event', start);
 			// ↓stopイベントは作らなくても良くない？
 			//$img.bind('start', start).bind('stop', stop);
-
+			
+			$('#prev').click(function(event) {
+				event.preventDefault();
+				$image.trigger('prev_event');
+			});
+		
+			$('#next').click(function(event) {
+				event.preventDefault();
+				$image.trigger('next_event');
+			});
+			
+			$image.trigger('goto_event', 0);
+			$image.trigger('start_event');
+*/
 		});
 
 		return this;
